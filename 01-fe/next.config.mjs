31c61fac930499f82a26f0b42021c6ef20da93ca
zany49 <abdul.k@doodle-blue.com>
2025-07-21 10:29:28 +0530

    removed netlify config

--- 01-fe/next.config.mjs
+++ 01-fe/next.config.mjs
@@ -2,12 +2,7 @@
 const nextConfig = {
      env: {
         NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    },
-    exportPathMap: async function () {
-    return {
-      '/': { page: '/' },
-    };
-  },
+    }
 };
 
 export default nextConfig;
